0dbc9d4d11a7138e62e43732f0349eb2040e9544
Daniel Holm <admin@danielholm.se>
2012-01-10 22:02:51 +0100

    Working indicator applet. Some cleanup.

--- bareshare.py
+++ bareshare.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #
 # BareShare
 # Authors: Daniel Holm, <admin@danielholm.se>, 120110
@@ -17,91 +18,140 @@
 # <http://www.gnu.org/licenses/>
 #
 
+# Imports needed modules
 import pygtk
 pygtk.require('2.0')
-import gobject
 import gtk
+import gobject
 import appindicator
+import pynotify
+import os
+
+# Settingsdir and -file.
+home = os.getenv('HOME')
+configdir = home + "/.bareshare"
+configfile = home + "/.bareshare/config.xml"
+
+# Check if config files and dirs exist. If not, create them.
+if not os.path.exists(configdir):
+	print "Config dir and file did not exist. Creating..."
+	os.makedirs(configdir)
+if not os.path.exists(configfile):
+	print "Configfile did not exist. Creating..."
+	open(configfile,'w').close()
 
 def menuitem_response(w, buf):
-  print buf
+	print buf
 
-class BareShareIndicator:
-# The about dialog
-    def show_about(widget):
-        about = gtk.AboutDialog()
-        about.set_program_name("BareShare")
-        about.set_version("0.1")
-        about.set_copyright("(c) Daniel Holm")
-        about.set_comments("Backup and Share Service")
-        about.set_website("https://github.com/danielholm/BareShare")
-        about.set_logo(gtk.set_icon("nautilus"))
-	about.set_license('GPL v3')
-        about.run()
-        about.destroy()
+
+# Creates the class for the application
+class BareShareAppIndicator:
+
+	def __init__(self):
+		self.ind = appindicator.Indicator ("BareShare", "nautilus", appindicator.CATEGORY_APPLICATION_STATUS)
+		self.ind.set_status (appindicator.STATUS_ACTIVE)
+		self.ind.set_attention_icon ("indicator-messages-new")
+		self.ind.set_icon("nautilus")
+
+		# Create a menu
+		self.menu = gtk.Menu()
+	
+        	# Dynamic label here
+
+		# Open Profile Creator
+		add = "Add Share"
+		add_share = gtk.ImageMenuItem(gtk.STOCK_ADD)
+		add_share.connect("activate", menuitem_response, add)
+		add_share.show()
+		self.menu.append(add_share)
+
+		# Create Submenu
+#		self.pmenu = gtk.Menu()
+
+#		importm = gtk.MenuItem("Profile")
+#		importm.set_submenu(self.pmenu)
+#		importm.show()
+		
+		# Item for adding new profiles
+#		addprofile = gtk.ImageMenuItem(gtk.STOCK_ADD)
+#		addprofile.connect("activate", self.show_filechooser, None)
+#		addprofile.show()
+#		self.pmenu.append(addprofile)
+
+		# Open Settings dialog
+		pref = "Preferences"
+		settings = gtk.ImageMenuItem(gtk.STOCK_PREFERENCES)
+		settings.connect("activate", menuitem_response, pref)
+		settings.show()
+		self.menu.append(settings)
+
+		# Separator
+		sep = gtk.SeparatorMenuItem()
+		sep.show()
+		self.menu.append(sep)
+
+		# Open About
+		about = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
+		about.connect("activate", self.show_about, None)
+		about.show()
+		self.menu.append(about)
+
+		# Quit-item
+		image = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+		image.connect("activate", self.quit)
+		image.show()
+		self.menu.append(image)
+                    
+		self.menu.show()
+
+		self.ind.set_menu(self.menu)
+
+	def quit(self, widget, data=None):
+		gtk.main_quit()
+
+# About
+	def show_about(self, widget, data):
+		# Create AboutDialog object
+		dialog = gtk.AboutDialog()
+
+		# Add the application name to the dialog
+		dialog.set_name('BareShare')
+
+		# Add a application icon to the dialog
+#		dialog.set_logo(gtk.gdk.pixbuf_new_from_file("abc.png"))
+#		dialog.set_logo(gtk.set_icon("nautilus"))
+
+		# Set the application version
+		dialog.set_version('0.1')
+
+		# Pass a list of authors.
+		dialog.set_authors('Soon')
+	        dialog.set_copyright("Daniel Holm")
+
+		# Add a short comment about the application.
+		dialog.set_comments('Backup and Share Service')
+
+		# Add license information.
+		license = "This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <http://www.gnu.org/licenses/>."
+		dialog.set_license(license)
+
+		# Add a homepage to the dialog.
+		dialog.set_website("https://github.com/danielholm/BareShare")
+
+		# Show the dialog
+		dialog.run()
+
+		# The destroy method must be called otherwise the 'Close' button will
+		# not work.
+		dialog.destroy()
+
+	def hide_dialog(self, widget, data):
+		widget.hide()
+
+def main():
+	gtk.main()
+	return 0
 
 if __name__ == "__main__":
-  indicator = appindicator.Indicator ("BareShare",
-                              "nautilus",
-                              appindicator.CATEGORY_APPLICATION_STATUS)
-  indicator.set_status (appindicator.STATUS_ACTIVE)
-  indicator.set_attention_icon ("indicator-messages-new")
-#gtk.set_from_image
-
-  # create a menu
-  menu = gtk.Menu()
-
-  # This should just be a name and then, when runing, show transfer speed and current file
-  label = "BareShare"
-  menu_label = gtk.MenuItem(label)
-  menu.append(menu_label)
-
-  # Add a new share/backup
-  new = "Add Share"
-  menu_add = gtk.MenuItem(new)
-  menu.append(menu_add)
-
-  # Preferences
-  pref = "Preferences"
-  menu_pref = gtk.ImageMenuItem(gtk.STOCK_PREFERENCES)
-  menu.append(menu_pref)
-
-  # Separator
-  item = gtk.SeparatorMenuItem()
-  item.show()
-  menu.append(item)
-
-  # About dialog
-  #about = "About" // Remove
-  menu_about = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
-  menu.append(menu_about)
-
-  # Separator
-  item = gtk.SeparatorMenuItem()
-  item.show()
-  menu.append(item)
-
-  #Quit button
-  menu_quit = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-  menu.append(menu_quit)
-
-  # Connect menu items to functions
-  menu_add.connect("activate", menuitem_response, new)
-  menu_pref.connect("activate", menuitem_response, pref)
-#  menu_about.connect("activate", show_about)
-  menu_quit.connect("activate", gtk.main_quit)
-
-  # show the items
-  menu_label.show()
-  menu_add.show()
-  menu_pref.show()
-  menu_about.show()
-  menu_quit.show()
-  menu.show()
-
-  indicator.set_menu(menu)
-
-#  about = AboutDialog()
-#  about.main()
-
-  gtk.main()
+	indicator = BareShareAppIndicator()
+	main()
